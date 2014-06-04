a354d16a743169ffee8835ac4a5c88307367529c
Tamreen Khan <historium@gmail.com>
2014-06-04 14:21:26 -0400

    Minor cosmetic changes.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function(){
     var ws = new WebSocket("ws://localhost:8000/repl");
 
-    var $console = $('#console').jqconsole('Hi\n', '>>> ', "...");
+    var $console = $('#console').jqconsole('', '>>> ');
     var startPrompt = function(){
         $console.Prompt(true, function(input){
             ws.send(input);
@@ -13,13 +13,12 @@ $(function(){
     startPrompt();
 
     ws.onmessage = function(event){
-        console.log(event.data);
         var data = event.data;
         if(data.substr(0, 8) === "<prompt>"){
             $console.SetPromptLabel(data.substr(8).trim() + " ");
             $console.SetPromptText("");
         }else{
-            $console.Write(event.data + '\n', 'jqconsole-output');
+            $console.Write(event.data, 'jqconsole-output');
         }
     }
 });
